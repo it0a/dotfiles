cbebde5d1a956348600e402cde8f601ea311a0c0
it0a <recursive404@gmail.com>
2014-10-29 19:55:43 -0400

    Removing xmonad.hs that would have already been there via install script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 pushd `dirname $0` > /dev/null
 SCRIPTPATH=`pwd -P`
 popd > /dev/null
+rm -f $HOME/.xmonad/xmonad.hs
 ln -s $SCRIPTPATH/xmonad.hs $HOME/.xmonad/xmonad.hs
 echo "Symlinked xmonad.hs to $HOME/.xmonad/xmonad.hs"
