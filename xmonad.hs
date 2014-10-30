ce1e19aee15cd4f6580460dd2ec3ba85184e584b
it0a <recursive404@gmail.com>
2014-10-29 20:10:04 -0400

    Adding borderWidth of zero to xmonad config

--- xmonad.hs
+++ xmonad.hs
@@ -2,6 +2,7 @@ import XMonad
  
 main = xmonad defaultConfig
         { modMask = mod4Mask -- Use Super instead of Alt
+        , borderWidth = 0
         , terminal = "urxvt"
         -- more changes
         }
