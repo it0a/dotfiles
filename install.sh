94c125759ac8452bec152cb373600ca8e8b4bc52
it0a <recursive404@gmail.com>
2015-05-29 19:22:56 -0400

    Minor syntax tweaks

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 pushd `dirname $0` > /dev/null
 SCRIPTPATH=$(pwd -P)
 popd > /dev/null
-for file in `find . -type f ! -path "./.git/*" ! -name "README.md" ! -name "install.sh" `; do
+for file in `find . -type f \
+    ! -path "./.git/*"      \
+    ! -name "README.md"     \
+    ! -name "install.sh" `; do
     ln -nsf `echo "$file" | sed "s|^\.|$SCRIPTPATH|g"` `echo "$file" | sed "s|^\.|$HOME|g"`
 done
 
